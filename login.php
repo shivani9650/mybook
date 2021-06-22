1ed0f2aada0e6fe8996ccb96de1f516fff782394
shivani <shivanijha25102000@gmail.com>
2021-06-22 23:53:53 +0530

    Create login.php

---
+++ login.php
@@ -0,0 +1,89 @@
+<html>
+		<head> 
+		<title>Mybook   login</title>
+		</head>
+		<style>
+         #bar{ 
+         	height:100px;
+          background-color: rgb(59,89,152);
+           color: #d9dfeb;
+            padding:4px;
+        }
+         #Signup_button{
+         	background-color: #42b72a;
+         	width: 70px;
+         	text-align: center;
+         	padding: 4px;
+         	border-radius: 4px;
+         	float:right;
+         }
+         #bar2{
+           background-color: white;
+            width:800px;
+             height:200px;
+              margin:auto;
+              margin-top:50px;
+        	padding:10px;
+        	padding-top: 50px;
+        	text-align: center;
+            font-weight: bold;
+               
+         }
+         #text{
+
+         	height: 40px;
+         	width: 300px;
+         	border-radius: 4px;
+         	border: solid 1px #ccc;
+         	padding: 4px;
+         	font-size: 14px;
+
+
+         }
+         #button{
+
+         	width: 300px;
+         	height: 40px;
+         	border-radius: 4px;
+    
+         	border: none;
+         	background-color: rgb(59,89,152);
+         	color: white;
+         		font-weight: bold;
+         }
+
+			
+		</style>
+
+
+
+		<body style="font-family:tahoma;background-color: #e9ebee;">
+			<div id="bar">
+	       <div style="font-size: 40px;">Mybook</div>
+	          <div id= "Signup_button">Signup</div>
+              <br><br>
+			</div>
+			
+        <div  id ="bar2">
+        	Log in to Mybook<br><br>
+        	<input type = "text"; id="text" placeholder="Email"><br><br>
+        	<input type = "password"; id="text" placeholder="password"><br><br>
+        	<input type = "submit"; id="button" value="Log in">
+
+
+        	   
+        	
+
+        	
+        </div>
+		</body>
+
+
+
+
+
+
+
+
+
+</html>
